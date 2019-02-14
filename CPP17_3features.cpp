e8c05a8c9959407e5032a90f6300211040bc5093
QuentinUK <QuentinUK@live.co.uk>
2019-02-14 15:56:30 +0000

    Update CPP17_3features.cpp

--- CPP17_3features.cpp
+++ CPP17_3features.cpp
@@ -5,26 +5,59 @@
 #include <tuple>
 #include <utility>
 #include <map>
+/*#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
 
+template<typename T>
+T pow_m(int n, int pow){
+	T p { n };
+	while(--pow) p*=n;
+	return p;
+}
+
+void power_test(){
+	cpp_int z, y, g = 0; 
+    const int rows = 20; 
+	for(int i=0; i<rows; i++) { 
+		y = 4 + g; 
+        z = y * y; 
+        g = g + 3*pow_m<cpp_int>(10, i+1);
+        
+		for(int m=rows;m>=i;m--) { 
+			std::cout << ' '; 
+		} 
+		std::cout << y << "’" << '=' << z << std::endl; 
+	} 
+}
+*/
 std::tuple<char, int, bool> mytuple()
 {
     char a = 'a';
     int i = 123;
     bool b = true;
-    return std::make_tuple(a, i, b);
+    return std::tuple(a, i, b); // make_tuple not needed anymore
 }
 
 int main() {
+    // power_test();
     // structured binding
     auto [a, i, b] = mytuple();
     std::cout << a << ' ' << i << ' ' << b << '\n';
     
     // returning compound objects
-    auto mymap{std::map<int, int>()};
+    auto mymap{std::map<char, int>()};
     auto [itelem, success] = mymap.insert(std::pair('a', 100));
     if (!success) {
         std::cout << "not inserted\n"; // Insert failure
     }
-	// your code goes here
+    // selection initialsation
+    if (auto [itelem, success] = mymap.insert(std::pair('b', 100)); success) {
+        std::cout << "well inserted\n"; // Insert success
+    }
+    for(const auto& [key, value] : mymap) {
+        std::cout << key << ' ' << value << '\n';
+     }
+	// direct initialisation
+    auto [a1, i1, b1] = std::tuple('a', 123, true);
 	return 0;
 }
